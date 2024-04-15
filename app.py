52dfbda566bc9b0e1da7e8aa10a6d622ad6583ae
Emir Şener <emir@cbot.ai>
2024-04-15 17:51:52 +0300

    deneme

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from routes.cbert import route
+import os
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:30720"
+
 
 app = Flask(__name__)
 app.register_blueprint(route, url_prefix='/cbert')
