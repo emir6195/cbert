4e53de9cbf2668dad70f556f3f4a4ea38bbebd09
Emir Şener <emir@cbot.ai>
2024-04-15 11:26:43 +0300

    overwrite

--- lib/CBERT.py
+++ lib/CBERT.py
@@ -6,28 +6,35 @@ class CBERT:
     def __init__(self) -> None:
         self.RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
-    def train(self, docs: list[str], index_name: str, meta_datas : list[dict], overwrite_index=True) -> bool:
-        try : 
-            self.RAG.index(
-                index_name=index_name,
-                collection=docs,
-                document_metadatas=meta_datas
-            )
-            return True
-        except Exception as e :
+    def train(self, docs: list[str], index_name: str, meta_datas: list[dict], overwrite_index=True) -> bool:
+        try:
+            if overwrite_index:
+                self.RAG.index(
+                    index_name=index_name,
+                    collection=docs,
+                    document_metadatas=meta_datas
+                )
+                return True
+            else:
+                self.RAG.add_to_index(
+                    index_name=index_name,
+                    collection=docs,
+                    document_metadatas=meta_datas
+                )
+        except Exception as e:
             print(e)
             return False
-    
-    def invoke(self, index_name: str, query: str) : 
-        try: 
+
+    def invoke(self, index_name: str, query: str):
+        try:
             return self.RAG.search(
                 query=query,
                 index_name=index_name,
                 k=4
             )
-        except Exception as e :
+        except Exception as e:
             print(e)
             return False
-        
+
 
 cbert = CBERT()
--- routes/cbert.py
+++ routes/cbert.py
@@ -6,11 +6,12 @@ route = Blueprint('cbert', __name__)
 @route.route('/train', methods=["POST"])
 def train():
     data = request.get_json()
-    if 'docs' in data and 'index_name' in data and 'meta_datas' in data:
+    if 'docs' in data and 'index_name' in data and 'meta_datas' in data and 'overwrite_index' in data:
         docs = data['docs']
         index_name = data['index_name']
         meta_datas = data['meta_datas']
-        success = cbert.train(docs, index_name, meta_datas)
+        overwrite_index = data['overwrite_index']
+        success = cbert.train(docs, index_name, meta_datas, overwrite_index)
         return jsonify({"success": success})
     else:
         return abort(400)
