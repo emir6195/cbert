83b9ba89967f1a4cf229728af274e5674e11c294
Emir Şener <emir@cbot.ai>
2024-04-15 15:14:46 +0300

    deneme

--- lib/CBERT.py
+++ lib/CBERT.py
@@ -16,6 +16,7 @@ class CBERT:
                     index_name=index_name,
                     collection=docs,
                     document_metadatas=meta_datas,
+                    split_documents=True
                 )
                 return True
             else:
@@ -23,6 +24,7 @@ class CBERT:
                     index_name=index_name,
                     new_collection=docs,
                     new_document_metadatas=meta_datas,
+                    split_documents=True
                 )
         except Exception as e:
             print(e)
